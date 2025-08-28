757b2ed4ca91d2ed56d3353390e0b689eaca9c8d
Ignacio W <ignaciowuilloud@gmail.com>
2025-08-27 21:29:12 -0300

    LULI

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const boton = document.getElementById("boton");
 const tbody = document.getElementById("tbody");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const imagen = document.getElementById("image");
+
 
 boton.addEventListener("click", (e) => {
     e.preventDefault(); // Previene el envío del formulario
     alert("DATOS ACTUALIZADOS!!!!!!!!!!!!!!!");
-    
+
+    let nombre1 = nombre.value;
+    let apellido1 = apellido.value;
+    let email1 = email.value;
+    if (nombre1 == "Lucia"){
+        image.innerHTML = `<img  src="fernet.png" style="max-width: 20%"></img>
+        <img  src="taxi.jpeg" style="max-width: 20%"></img>`
+    }
+
     tbody.innerHTML = `
         <tr>
-            <td>Juan</td>
-            <td>Wuilloud</td>
+            <td>${nombre1}</td>
+            <td>${apellido1}</td>
             <td>20</td>
             <td>Aries</td>
-            <td>igna@yopmail.com</td>
+            <td>${email1}</td>
             <td>2612514127</td>
             <td>🧑🏼‍🦰Colorado🧑🏼‍🦰</td>
+            <td>🦊🦊</td>
         </tr>
     `;
 });
