7af24e462f465a2dd6c338ef11ed361f6883f327
Ignacio W <ignaciowuilloud@gmail.com>
2025-08-27 21:13:03 -0300

    more changes

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const botoncito = document.getElementById("botoncito");
-botoncito.addEventListener("click", () => {
-    alert("🎉🎊🥳");
-})
+const boton = document.getElementById("boton");
+const tbody = document.getElementById("tbody");
+
+boton.addEventListener("click", (e) => {
+    e.preventDefault(); // Previene el envío del formulario
+    alert("DATOS ACTUALIZADOS!!!!!!!!!!!!!!!");
+    
+    tbody.innerHTML = `
+        <tr>
+            <td>Juan</td>
+            <td>Wuilloud</td>
+            <td>20</td>
+            <td>Aries</td>
+            <td>igna@yopmail.com</td>
+            <td>2612514127</td>
+            <td>🧑🏼‍🦰Colorado🧑🏼‍🦰</td>
+        </tr>
+    `;
+});
